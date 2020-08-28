7554177b0e5a72bab540327dbc09ee3c2db24f4d
pramod <pramod@open-insights.com>
2020-08-28 07:41:56 +0530

    initializing data via data.sql file

--- src/main/java/com/springframework/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/com/springframework/msscbeerservice/bootstrap/BeerLoader.java
@@ -1,11 +1,10 @@
 package com.springframework.msscbeerservice.bootstrap;
 
-import com.springframework.msscbeerservice.domain.Beer;
 import com.springframework.msscbeerservice.repositories.BeerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
+import java.util.UUID;
 
 @Component
 public class BeerLoader implements CommandLineRunner {
@@ -15,6 +14,10 @@ public class BeerLoader implements CommandLineRunner {
     public static final String BEER_1_UPC = "0631234200036";
     public static final String BEER_2_UPC = "0631234300019";
     public static final String BEER_3_UPC = "0083783375213";
+    public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
+    public static final UUID BEER_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
+    public static final UUID BEER_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
+
 
     public BeerLoader(BeerRepository beerRepository) {
         this.beerRepository = beerRepository;
@@ -22,39 +25,7 @@ public class BeerLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        loadBeerObject();
-    }
-
-    private void loadBeerObject() {
-        if(beerRepository.count()==0)
-        {
-            beerRepository.save(Beer.builder()
-                    .beerName("Mango")
-                    .beerStyle("ILP")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_1_UPC)
-                    .price(new BigDecimal("12.95"))
-                    .build());
-
-            beerRepository.save(Beer.builder()
-                    .beerName("Orange")
-                    .beerStyle("ILP")
-                    .quantityToBrew(300)
-                    .minOnHand(10)
-                    .upc(BEER_2_UPC)
-                    .price(new BigDecimal("11.95"))
-                    .build());
-
-            beerRepository.save(Beer.builder()
-                    .beerName("Apple")
-                    .beerStyle("IDP")
-                    .quantityToBrew(400)
-                    .minOnHand(10)
-                    .upc(BEER_3_UPC)
-                    .price(new BigDecimal("10.95"))
-                    .build());
-
-        }
+        //using data.sql file now
+        //loadBeerObject();
     }
 }
